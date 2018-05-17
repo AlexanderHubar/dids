1a46ef18b0f70e54a342bbd7de2956d50c367f1b
AlexThunder <thunderson28@gmail.com>
2018-05-17 14:03:39 +0300

    Initial commit

---
+++ dids.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+	function DidsOR (did, delayStart, play) {
+		$(did).delay(delayStart).queue(function () { 
+			$(this).css('display', 'block'); 
+			$(this).dequeue();
+		}).delay(play).queue(function () { 
+			$(this).css('display', 'none'); 
+			$(this).dequeue();
+		})
+	}
+	function playMusic () {
+		$(".doOR").add( "<audio id='audio' src='audio.mp3' volume='1' loop autoplay></audio>" );
+	}
+	$(".doOR").click(function() {
+		playMusic();
+		DidsOR(".did1", 200, 3200);
+		DidsOR(".did2", 3900, 3500);
+		DidsOR(".did3", 7400, 3800);
+		DidsOR(".did4", 11200, 3800);
+		DidsOR(".did5", 15000, 4800);
+		DidsOR(".did6", 19800, 2800);
+		DidsOR(".did7", 24900, 1300);
+		DidsOR(".did8", 26700, 3500);
+	})
+});
